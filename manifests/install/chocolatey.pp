49a16c5e38bfe38d1fef70973e001472bbedcc99
Daniel Henninger <daniel@ncsu.edu>
2018-10-04 13:31:26 -0400

    Trying to implement the cleanest option I can for holding at a particular version.

--- manifests/install/chocolatey.pp
+++ manifests/install/chocolatey.pp
@@ -1,7 +1,14 @@
 class winlogbeat::install::chocolatey {
+  if $::winlogbeat::package_ensure == 'held' {
+    $install_opts = ['--version', $::winlogbeat::package_version]
+  }
+  else {
+    $install_opts = undef
+  }
   package {$::winlogbeat::package_name:
-    ensure   => $::winlogbeat::package_ensure,
-    source   => $::winlogbeat::package_source,
-    provider => 'chocolatey',
+    ensure          => $::winlogbeat::package_ensure,
+    source          => $::winlogbeat::package_source,
+    install_options => $install_opts,
+    provider        => 'chocolatey',
   }
 }
